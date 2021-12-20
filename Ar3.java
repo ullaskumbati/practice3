4ab74322628436627cec3793752cc2501964d3f6
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:13:10 +0530

    added Ar3

---
+++ Ar3.java
@@ -0,0 +1,14 @@
+class Ar3{
+	public static void main(String []as){
+		int []cat= {3456,10345,15034,50405,102345,23456,758693,567854,33453456};
+		System.out.println(cat[3]);
+		System.out.println(cat[6]);
+		System.out.println(cat[8]);
+		System.out.println(cat[1]);
+		System.out.println(cat[4]);
+		System.out.println(cat[5]);
+		System.out.println(cat[2]);
+		System.out.println(cat[7]);
+		System.out.println(cat[2]);
+	}
+}
